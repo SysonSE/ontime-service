8a8300318a2b14c2c9b43ce23d39337af9c4e20d
Soroush Hakami <soroush.hakami@gmail.com>
2013-10-06 17:11:46 +0200

    added spec runner for better display of test results

--- test/api.js
+++ test/api.js
@@ -1,4 +1,4 @@
-/* global describe:true, it:true, before:true*/
+/* global describe:true, it:true*/
 "use strict";
 var request = require('superagent');
 var expect = require('expect.js'); //expect-tyle assertions
@@ -6,8 +6,8 @@ var assert = require('assert'); //assert-style assertions
 var app = require('../app');
 var port = app.get('port');
 
-describe('API test suite 1', function(){
-	describe('root of domain (/)', function(){
+describe('API tests', function(){
+	describe('/ (root url)', function(){
 
 		it('should return a 200 statuscode', function(done){
 			request.get('localhost:'+port).end(function(res){
@@ -16,10 +16,21 @@ describe('API test suite 1', function(){
 			});
 		});
 
-		it('should return a json-object describing the service', function(done){
+		it('should contain routes', function(done){
 			request.get('localhost:'+port).end(function(res){
 				expect(res).to.exist;
-				expect(res.text).to.contain('ontime');
+				var jsonResponse = JSON.stringify(res.text);
+				expect(jsonResponse.routes).to.exist;
+				done();
+			});
+		});
+	});
+
+	describe('/companies', function(){
+
+		it('should return a 200 statuscode', function(done){
+			request.get('localhost:'+port+'/companies').end(function(res){
+				assert.equal(200, res.status);
 				done();
 			});
 		});
