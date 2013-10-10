f96cf5a004a4cf60add7b6f37770e1fb58508ac3
Soroush Hakami <soroush.hakami@gmail.com>
2013-10-10 19:40:13 +0200

    auth now applies to all paths except the root / path

--- app.js
+++ app.js
@@ -10,24 +10,12 @@ var express = require('express'),
 	companyApi = require('./lib/company-api'),
 	userApi = require('./lib/user-api');
 
-/* Mount subapplications */
-app.use(loginApi);
-app.use(companyApi);
-app.use(userApi);
+app.get('/', function(req, res){
+    res.send('Hello says the Ontime Service.');
+});
 
-app.set('port', process.env.PORT || 3000);
-app.use(express.logger('dev'));
-app.use(express.methodOverride());
-
-if (env === 'development') {
-	app.use(express.errorHandler());
-}
-/*
 app.all('*', function(req, res, next){
 	if(_.contains(config.allowedHosts, req.host)){
-
-        console.log("auth passed");
-
 		next();
 	} else {
 		console.log('request from hostname: ' + req.host + ' not allowed.');
@@ -37,23 +25,42 @@ app.all('*', function(req, res, next){
 		});
 		next(new Error(401));
 	}
-});*/
-
-app.get('/', function(req, res){
-
-    console.log("get root");
-
-    res.send(JSON.stringify({
-        routes: {
-            GET: app.routes['get'],
-            POST: app.routes['post'],
-            PUT: app.routes['put'],
-            DELETE: app.routes['delete']
-        }
-    }));
 });
+
+app.use(express.basicAuth(function(user, pass, fn) {
+	fakeDatabaseLookup(user, pass, fn);
+}));
+
+/* Mount subapplications */
+app.use(loginApi);
+app.use(companyApi);
+app.use(userApi);
+
+app.set('port', process.env.PORT || 3000);
+app.use(express.logger('dev'));
+app.use(express.methodOverride());
+app.use(express.bodyParser());
+
+if (env === 'development') {
+	app.use(express.errorHandler());
+}
 
 http.createServer(app).listen(app.get('port'), function(){
 	console.log('OnTime service listening on port ' + app.get('port'));
 });
 
+function fakeDatabaseLookup(user, pass, fn){
+	/*
+		fn är en callback-function som vi behöver invokera för att tala om för 
+		express att autentiseringen lyckades (true) eller misslyckades (false).
+		Varför man passar in 'null' först är för att express använder konventionen 
+		(liksom de flesta node-rameverk)
+		att alla callbacks tar först ett error-argument (eller null om det inte existerar),
+		följt av resten av argumenten. 
+	*/
+	if(user === 'jdog' && pass === 'hitler'){
+		fn(null, true);
+	} else {
+		fn(null, false);
+	}
+}
--- test/apiTest.js
+++ test/apiTest.js
@@ -13,18 +13,12 @@ describe('API tests', function(){
 			request(app).get('/').expect(200, done);
 		});
 
-		it('should contain routes', function(done){
-			request(app).get('/').expect(200).end(function(err, res){
-				expect(JSON.parse(res.text).routes).to.be.ok();
-				done();
-			});
-		});
 	});
 
 	describe('/companies', function(){
 
 		it('should return a 200 statuscode', function(done){
-			request(app).get('/companies').expect(200, done);
+			request(app).get('/companies').auth(username, password).expect(200, done);
 		});
 	});
 
@@ -40,17 +34,17 @@ describe('Authentication', function(){
 
 	describe('Failing auth', function(){
 		it('should fail when no credentials set', function(done){
-			request(app).get('/login').expect(401, done);
+			request(app).post('/login').expect(401, done);
 		});
 
 		it('should fail when wrong credentials set', function(done){
-			request(app).get('/login').auth('derp', 'flerp').expect(401, done);
+			request(app).post('/login').auth('derp', 'flerp').expect(401, done);
 		});
 	});
 
 	describe('Successful auth', function(){
 		it('should succeed with status code 200', function(done){
-			request(app).get('/login').auth(username, password).expect(200, done);
+			request(app).post('/login').auth(username, password).expect(200, done);
 		});
 	});
 
